edffb7850b61dc0613f14dbbe9f7cc23ea83b4d6
Ozomo <emil000320@gmail.com>
2024-06-21 00:34:06 +0200

    .

---
+++ bingo/script.js
@@ -0,0 +1,7 @@
+function changeColor(btnId, txtId){
+    let button = document.getElementById(btnId);
+    let text = document.getElementById(txtId);
+    button.style.backgroundColor = "grey";
+    text.style.color = "white"
+}
+
